4e03668427117de1a74d22aabaad7d40525fc4d9
Daniel Viramontes <danielviramo@gmail.com>
2020-11-16 23:03:41 -0600

    Code

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -29,5 +29,24 @@ function checkDatebase(){
     const store = transaction.objectStore("pending");
     const getAll = store.getAll();
 
-    getAll.onsuccess = function()
+    getAll.onsuccess = function(
+        if(getAll.result.length > 0) {
+            fetch("/api/transaction/bulk",{
+                method:"POST",
+                body: JSON.stringify(getAll.result),
+                header:{
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type":"application.json"
+                }
+            })
+            .then (response => response.json())
+            .then (() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            });
+        }
+    );
 }
+
+window.addEventListener("online", checkDatabase);
