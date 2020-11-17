954405506f93785d623eee3a3d4e79151a61281b
Daniel Viramontes <danielviramo@gmail.com>
2020-11-16 23:34:29 -0600

    Code

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -24,4 +24,28 @@ self.addEventListener("install", function(evt) {
       self.skipWaiting();
     });
 
-    self.
+    self.addEventListener("activate", function(evt) {
+        evt.waitUntil(
+          caches.keys().then(keyList => {
+            return Promise.all(
+              keyList.map(key => {
+                if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                  console.log("Removing old cache data", key);
+                  return caches.delete(key);
+                }
+              })
+            );
+          })
+        );
+      
+        self.clients.claim();
+      });
+
+
+    self.addEventListener("fetch", function(evt){
+        if (evt.request.url.includes("/api/")) {
+            evt.respondWith(
+                caches.open(DATA_CACHE_NAME).then
+            )
+        }
+    })
